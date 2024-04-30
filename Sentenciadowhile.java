5233619f2315f33d725540f7671f7924bc0a26c6
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-04-29 21:01:25 -0600

    Create Sentenciadowhile.java

---
+++ Sentenciadowhile.java
@@ -0,0 +1,12 @@
+public class Sentenciadowhile {
+    public static void main(String[] args) {
+        int i = 0;
+    do{
+       System.out.println("valor de i = \n"+ i);
+        i++;
+    } while(i<3);
+
+System.out.println("Fin");
+    }
+    
+}
